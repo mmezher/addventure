b6cfb154048238dd5939d284827b25c328574353
mmezher <mmm5fd@virginia.edu>
2016-07-08 09:23:31 -0400

    Worked in helper module for headers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#Will return basic title if title isn''t provided
+	def base_title(page_title='')
+		base_title = "Addventure"
+		if page_title.empty?
+			base_title
+		else
+			page_title + "|" + base_title
+		end
+	end
 end
