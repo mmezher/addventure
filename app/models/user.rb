90d4862b8c0f5b5e4ff066c990ecc43647eab840
mmezher <mmm5fd@virginia.edu>
2016-07-20 08:29:06 -0400

    Set up user databases and began sign up page

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	before_save { self.email = email.downcase }
+	validates :name, presence: true, length: { maximum: 50}
+	val_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
+	validates :email, presence: true, length: {maximum: 60}, format: { with: val_email_regex }, uniqueness: {case_sensitive: false}
+	has_secure_password
+	validates :password, presence: true, length: {minimum: 6}
+	end
