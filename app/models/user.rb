871cb42d431134a0b5c51421d1f260ec0fff6ad6
mmezher <mmm5fd@virginia.edu>
2016-08-08 09:38:16 -0400

    advanced login with hashed tokens, remember me capabilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50}
 	val_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
@@ -14,4 +15,25 @@ def User.digest(string)
 
 	BCrypt::Password.create(string, cost:cost)
 	end
+#Returns random tokens
+def User.new_token
+	SecureRandom.urlsafe_base64
 end
+
+#Remembers user in database for use in cookie storage
+def remember
+	self.remember_token = User.new_token
+	update_attribute(:remember_digest, User.digest(remember_token))
+end
+
+#returns true if given token matches digest
+def authenticated?(remember_token)
+	return false if remember_digest.nil?
+	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+end 
+
+def forget
+	update_attribute(:remember_digest, nil)
+end
+end
+
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	@user = users(:monique)
   end
 
+  test "login with remembering" do 
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+    assert_not_nil cookies['remember_token']
+  end
+
+  test "login without remembering" do 
+    log_in_as(@user, remember_me: '0')
+    assert_nil cookies['remember_token']
+  end
+
   test "login with invalid information" do 
   	get login_path
   	assert_template "sessions/new"
@@ -28,7 +39,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	delete logout_path
   	assert_not is_logged_in?
   	assert_redirected_to root_url
-  	follow_redirect!
+  	#simulate user clicking to logout of second window
+    delete logout_path
+    follow_redirect!
   	assert_select "a[href=?]", login_path
   	assert_select "a[href=?]", logout_path, count: 0
   	assert_select "a[href=?]", user_path(@user), count: 0
