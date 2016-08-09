50266a64c2243349cb3e3ad11a00fb3153005b42
mmezher <mmm5fd@virginia.edu>
2016-08-09 10:49:31 -0400

    Working administrator rights, user visible database. Need to fix bug with pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+before_action :logged_in_user, only: [:edit, :update, :index, :destroy]
+before_action :correct_user, only: [:edit, :update]
+before_action :admin_user, only: :destroy 
 def new
 	@user = User.new
 end
@@ -12,17 +14,61 @@ def create
 	if @user.save 
 		log_in @user
 		flash[:success] = "Welcome to AddVenture!"
-		redirect_to [@user]
-		
-		
+		redirect_to @user
 	else
 		render 'new'
 	end
+end
+
+def edit
+end
+
+def destroy
+
+User.find(params[:id]).destroy
+flash[:success] = "User deleted"
+redirect_to users_url
+end
+
+def update
+	if
+		@user.update_attributes(user_params)
+		flash[:success] = "New changes saved successfully"
+		redirect_to @user
+		#do success stuff
+	else
+		render 'edit'
+	end
+end
+
+def index
+	@users = User.paginate(page: params[:page])
 end
 
 private
 
 def user_params
 	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
+
+def logged_in_user
+	unless logged_in?
+		store_location
+		flash[:danger] = "Please sign in to access this page"
+		redirect_to login_url
+	end
+end
+
+def correct_user
+	@user = User.find(params[:id])
+	redirect_to(root_url) unless current_user?(@user)
+
+end
+
+def admin_user
+	flash[:danger] = "You don't have access to do that at this time."
+	redirect_to(root_url)
+	unless current_user.admin?
+	end
 end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ActiveRecord::Base
 	val_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
 	validates :email, presence: true, length: {maximum: 60}, format: { with: val_email_regex }, uniqueness: {case_sensitive: false}
 	has_secure_password
-	validates :password, presence: true, length: {minimum: 6}
-
+	validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 #returns hash digest of given string
 def User.digest(string)
 	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : 
