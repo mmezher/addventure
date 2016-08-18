42d3bc9c1c41da3c6b20ce80ea89d8f69eabc25e
mmezher <mmm5fd@virginia.edu>
2016-08-17 21:34:02 -0400

    Done password creation reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,10 +49,7 @@ def create_reset_digest
 	update_attribute(:reset_sent_at, Time.zone.now)
 end
 
-def create_reset_digest
-	self.reset_token = User.new_token
-	update_columns(reset_digest: FILL_IN, reset_sent_at: FILL_IN)
-end 
+
 def send_password_reset_email
 	UserMailer.password_reset(self).deliver_now
 end  
